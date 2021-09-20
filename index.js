4266ff058cd6f596ed4ad2a9cc8b6ae81bb3503a
Fernando Vargas <fernando.vargas@asksuite.com>
2021-09-20 13:37:17 -0300

    fix: remove bodyparser deprecated

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const express = require("express");
 const app = express();
 const { DateTime } = require("luxon");
 const { roomSearch } = require("./services/BrowserService");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
